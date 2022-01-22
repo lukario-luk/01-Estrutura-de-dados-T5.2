f1cc91e502faaf155f26434c84bf9e18c2e634a6
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-01-21 21:25:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = "lucas"
+let idade = 21
+let ano = 2022 - idade
+console.log (idade+ ano)    
+console.log ("Estou mexendo no console.log")
+let quartaFeira = false
+console.log (typeof quartaFeira)
